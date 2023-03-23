1e7d239ad9835c8e5680ad47214534f7eae6ef0c
dianavodopivec <dianavodopivec16@gmail.com>
2023-03-23 11:44:19 -0300

    tareacompleta

--- practice.js
+++ practice.js
@@ -1,13 +1,79 @@
+// (1) Varios LET con Distintos Datos Primitivos //
 
-let colorDeUñas = "rositas"
-colorDeUñas = "amarillo"
-console.log (colorDeUñas)
+let colorDePantalon = ("Negro")
+colorDePantalon = "Azul"
+console.log (colorDePantalon)
 
-const direccionCasa = 128
-console.log (direccionCasa)
+let cantidadDeManzanas = (4)
+cantidadDeManzanas = 7
+console.log (cantidadDeManzanas)
 
-let mascota = "PERRITO"
-let edadMascota = 1 
-let comioHoy = true
-console.log (mascota)
+let estaLloviendo = false
+estaLloviendo = true
+console.log (estaLloviendo)
 
+let ireAEstudiar = undefined
+ireAEstudiar = undefined
+console.log (ireAEstudiar)
+
+// (2) Varios CONST con distintos tipos de Datos Primitivos //
+
+const colorDePiel = ("Blanco")
+console.log (colorDePiel)
+
+const fechaDeNacimiento = (17112000)
+console.log (fechaDeNacimiento)
+
+const ojosVerdes = true
+console.log (ojosVerdes)
+
+// (3) 2 Objetos con datos primitivos //
+
+let sobreMi = {
+    nombre: "Diana",
+    edad: 23,
+    nacionalidad: "Argentina",
+    estaEstudiando: true,
+    iraAlDentista: undefined
+}
+
+console.log (sobreMi)
+
+let miMascota = {
+    nombreDeMascota: "Vex",
+    edadDeMascota: 1,
+    razaDeMascota: "Mestizo",
+    estaCastrada: false,
+    tieneSueño: undefined
+}
+
+console.log (miMascota)
+
+// (4) 2 Array //
+
+let planetas = ["marte", "jupiter", "saturno", "neptuno"]
+console.log (planetas)
+
+let planetas1 = ["jupiter", "saturno", "neptuno", "marte"]
+console.log (planetas1)
+
+// (5) Objeto + Propiedades (datos primitivos) + objeto + Array //
+
+let Gato = {
+    nombre: "Raul",
+    edad: 6,
+    seBaño: false,
+    comioHoy: undefined,
+    dondeEsta: null, 
+    suAmigaGatuna: {
+        nombre: "Lucy",
+        raza: "Mestiza",
+        coloresDelGato: ["negro", "blanco", "marrón", "rosado"]
+    }
+}
+
+console.log (Gato)
+
+// Typeof de Objeto anterior //
+
+console.log (typeof Gato.suAmigaGatuna.coloresDelGato[0])
